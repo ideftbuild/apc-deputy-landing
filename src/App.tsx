aa6fbba9c76758a101a6cb5302db3ba8dc201172
ideftbuild <a.akanSWE@gmail.com>
2025-10-13 18:27:40 +0100

    feat: incoporate the changes to reflect new layout and structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,24 @@
 // src/App.tsx
 // Access components from window object
-
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import About from "./components/About";
 /**
  * The main application component.
  * This component orchestrates the layout of the entire landing page by importing and
  * arranging all the modular section components.
  */
 
-window.App = function App() {
+export default function App() {
 
   return (
     <div className="">
       <Header />
-      <Navbar />
 
       <main>
-        <Hero />
-        <About />
-        <VisionMission />
-        <CoreValues />
-        <Attributes />
-        <Donation />
-        <SocialMedia />
-        <ContactForm />
+        <Hero/>
+        <About/>
       </main>
-
-      <Footer />
     </div>
   );
 };
