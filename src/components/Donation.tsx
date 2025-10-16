e61a5c52b79da783c6617eee7ca6ae528815a45e
ideftbuild <a.akanSWE@gmail.com>
2025-10-16 16:02:17 +0100

    refactor: enhance layout and styling

--- src/components/Donation.tsx
+++ src/components/Donation.tsx
@@ -11,15 +11,19 @@ export default function Donation() {
   return (
     <section
       id="donate"
-      className="py-16 md:py-20 flex flex-col justify-center items-center z-20 sticky"
+      className="py-16 md:py-20 flex flex-col items-center z-20 sticky h-screen"
     >
-      <div className="mx-auto px-6 w-[90%] md:w-[60%]">
-        {/*<HandHeart className="w-12 h-12 text-pink-500 mx-auto" />*/}
-        <h2 className="text-3xl md:text-4xl font-bold text-center text-gray-700 mb-12">
-          Support the Movement
-        </h2>
-
-        <div className="text-center p-2 text-gray-700">
+      {/*<HandHeart className="w-12 h-12 text-pink-500 mx-auto" />*/}
+      <h2 className="text-3xl text-center md:text-start md:text-4xl font-bold text-gray-700 mt-4 mb-12 w-[90%] md:w-fit">
+        Support the Movement
+      </h2>
+      <div className="flex flex-col md:flex-row items-center">
+        <img
+          src="/img/donate.webp"
+          alt="Donate"
+          className="w-full md:w-1/2 h-auto"
+        />
+        <div className="p-2 text-gray-700 text-center md:text-start">
           <p className="text-lg md:text-xl max-w-3xl mx-auto mb-8">
             Your generous contribution fuels our campaign, powers our grassroots
             outreach, and brings us one step closer to victory. Together, we can
@@ -27,7 +31,7 @@ export default function Donation() {
           </p>
           <a
             href={APP_CONSTANTS.donateLink}
-            className="inline-flex items-center gap-2 px-6 py-3 mt-4 bg-red-500 text-white font-semibold rounded-full shadow-md hover:bg-red-600 focus:ring-2 focus:ring-red-400 focus:outline-none transition-all duration-200"
+            className="inline-flex items-center gap-2 px-6 py-3 m-auto bg-red-500 text-white font-semibold rounded-full shadow-md hover:bg-red-600 focus:ring-2 focus:ring-red-400 focus:outline-none transition-all duration-200"
             aria-label="Contribute to the campaign"
           >
             Contribute Now
